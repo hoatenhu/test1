3be327fcf2972cf8d10386ff3f6b328c3a503f9d
hoatenhu <lenhuhoa1003@gmail.com>
2022-10-10 16:03:55 +0700

    init project

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, useColorScheme, View} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import codePush from 'react-native-code-push';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <View style={{height: 200, backgroundColor: 'green'}} />
+    </SafeAreaView>
+  );
+};
+
+export default codePush(App);
